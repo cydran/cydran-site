827b99d5edf5508b2509331b1c6ea9a23018069b
Burk Price <webigrator@gmail.com>
2018-07-13 08:43:53 -0600

    clean-up of site demo code

--- src/component/TutorialsComponent.ts
+++ src/component/TutorialsComponent.ts
@@ -2,15 +2,21 @@ import {Component} from "cydran";
 import TEMPLATE from "./TutorialsComponent.html";
 import CONTENT from "./TutorialsComponent.md";
 
-class DocsComponent extends Component {
+class TutorialsComponent extends Component {
 
 	private mdContent: string;
+	private myField: string;
 
 	constructor() {
 		super('tutorials', () => TEMPLATE);
 		this.mdContent = CONTENT;
+		this.myField = "Kilroy was here!";
+	}
+
+	public handleMyClick(): void {
+		window.alert(this.myField);
 	}
 
 }
 
-export default DocsComponent;
+export default TutorialsComponent;
