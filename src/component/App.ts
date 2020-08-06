6959a554cec2fffa796c8f5e272d225f904ef3ad
Matthew Werny <matthewwerny+github@gmail.com>
2020-08-05 22:25:10 -0600

    Updated to Cydran 0.1.36

--- src/component/App.ts
+++ src/component/App.ts
@@ -3,17 +3,24 @@ import TEMPLATE from "./App.html";
 
 class App extends Component {
 
+	private value: {
+		first: string;
+	};
+
 	constructor() {
 		super(TEMPLATE);
 		this.on("navigate").forChannel("navigation").invoke(this.navigate);
 		this.on(Events.AFTER_CHILD_CHANGED).invoke(this.onRegionChange);
+		this.value = {
+			first: "A parent value"
+		};
 	}
 
 	public navigate(name: string): void {
 		this.setChildFromRegistry("body", 'page:' + name, 'page:notFound');
 	}
 
-	public onRegionChange(payload: {name: string}): void {
+	public onRegionChange(payload: { name: string; }): void {
 		this.getLogger().ifTrace(() => "Updated region: " + payload.name);
 	}
 
